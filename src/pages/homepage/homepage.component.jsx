a6564768066da116024ee6e3d67f7615f4460981
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-07-09 20:22:26 +0530

    Separated components and pages

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from "../../components/directory/directory.component";
+// CSS
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
