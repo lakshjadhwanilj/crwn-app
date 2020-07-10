154ccfdd6662d899ffa93879ad4336de29d5fd00
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-07-10 13:02:23 +0530

    Added React Router and Shop page with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 // CSS
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
