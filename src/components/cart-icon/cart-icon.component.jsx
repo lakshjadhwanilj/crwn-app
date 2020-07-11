4b77811c77742060f58583cbfc0261c430d09484
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-07-12 01:55:59 +0530

    Updated cart icon, added reslect to use selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,10 +6,10 @@ import './cart-icon.styles.scss';
 // LOGO
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'> 0 </span>
+        <span className='item-count'> {itemCount} </span>
     </div>
 );
 
@@ -17,4 +17,8 @@ const mapDipatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDipatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+});
+
+export default connect(mapStateToProps, mapDipatchToProps)(CartIcon);
